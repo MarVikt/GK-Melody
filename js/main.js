aa8ead2195b040a030342b615ce456c1c6cae64f
MarVikt <95213605+MarVikt@users.noreply.github.com>
2021-12-12 19:37:52 +0300

    заставили работать кнопки выбора этажа

---
+++ js/main.js
@@ -0,0 +1,59 @@
+// проверим, готов ли сайт к манипуляциям
+$(document).ready(function () {
+    var currentFloor = 2;
+    var floorPath = $(".house path");
+    var counterUp = $(".counter-up");
+    var counterDown = $(".counter-down");
+ 
+console.log('сайт готов к манипуляциям');  
+
+// по клику на этаж
+// $(".house path").on("click", function () {
+//     console.log('кликнули на этаж'); 
+//     console.log($(this).attr("data-floor"));
+//     currentFloor = $(this).attr("data-floor");
+//     $(".counter").text(currentFloor);
+// })
+
+// по наведению мышкой на этаж
+floorPath.on("mouseover", function () {
+    floorPath.removeClass('current-floor');
+    currentFloor = $(this).attr("data-floor");
+    ascurrentFloor = currentFloor.toLocaleString('en-US',{minimumIntegerDigits: 2, 
+        useGrouping: false}); // форматируем в вид 03, а не просто 3
+    $(".counter").text(ascurrentFloor);
+    $(`[data-floor=${ascurrentFloor}]`).toggleClass("current-floor");; // назначили класс текущему path. А в стилях объявили видимость этого класса
+});
+
+counterUp.on("click", function () {
+    console.log('кликнули по кнопке выбора этажа вверх');
+    if (currentFloor < 18) {
+        currentFloor++;
+        ascurrentFloor = currentFloor.toLocaleString('en-US',{minimumIntegerDigits: 2, 
+        useGrouping: false});
+        $(".counter").text(ascurrentFloor);
+
+        floorPath.removeClass('current-floor');
+        // назначили path класс current-floor
+        $(`[data-floor=${ascurrentFloor}]`).toggleClass("current-floor");;
+    }
+});
+
+counterDown.on("click", function () {
+    console.log('кликнули по кнопке выбора этажа вниз');
+    if (currentFloor > 2) {
+        currentFloor--;
+        ascurrentFloor = currentFloor.toLocaleString('en-US',{minimumIntegerDigits: 2, 
+        useGrouping: false});
+        $(".counter").text(ascurrentFloor);
+
+        floorPath.removeClass('current-floor');
+        // назначили path класс current-floor
+        $(`[data-floor=${ascurrentFloor}]`).toggleClass("current-floor");;
+    }
+});
+
+
+
+
+});
